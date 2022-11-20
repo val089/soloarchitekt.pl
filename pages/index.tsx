4fdd2b023b85c2157f5c2600871e7f1b04b1c0c2
Kamil Szerląg <kamilszerlag@gmail.com>
2022-11-20 15:41:53 +0100

    feature/create-home-aboutMe-offer-pages

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,28 @@
+import Image from 'next/image';
 import { Layout } from '../components/Layout';
+import mainImgDesktop from '../assets/images/img-desktop.jpg';
+import classes from '../styles/HomePage.module.scss';
 
 const Home = () => {
   return (
     <Layout>
-      <p>HOME</p>
+      <section className={classes.home_introSection}>
+        <div className={classes.home_mainText}>
+          <h3>PRACOWNIA ARCHITEKTONICZNA</h3>
+          <h2>Pomagam Tobie Budować Marzenia i Daję Więcej niż oczekujesz</h2>
+        </div>
+        <div className={classes.home_mainInfo}>
+          <div className={classes.home_mainInfoItem}>
+            <span>100%</span>
+            <p>ZADOWOLONYCH KLIENTWÓW</p>
+          </div>
+          <div className={classes.home_mainInfoItem}>
+            <span>200</span>
+            <p>ZAKOŃCZONYCH PROJEKTÓW</p>
+          </div>
+        </div>
+        <Image src={mainImgDesktop} alt="main image" className={classes.home_mainImg} />
+      </section>
     </Layout>
   );
 };
