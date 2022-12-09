a73277e6e4bbe2c0abef39a9754503ff0712570e
Kamil Szerląg <kamilszerlag@gmail.com>
2022-12-09 23:04:12 +0100

    optimization

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -4,19 +4,35 @@ import classes from './Layout.module.scss';
 import { MenuProvider } from '../../hooks/useMenu';
 import { MenuOpenButton } from './MenuOpenButton';
 import { UnderConstruction } from '../UnderConstruction';
+import localFont from '@next/font/local';
+
+const centuryGothicFont = localFont({
+  src: [
+    {
+      path: '../../assets/fonts/CenturyGothic-Regular.woff2',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../../assets/fonts/CenturyGothic-Bold.woff2',
+      weight: '700',
+      style: 'normal',
+    },
+  ],
+});
 
 type LayoutProps = {
   children: ReactNode;
 };
 
 export const Layout = ({ children }: LayoutProps) => {
-  const isUnderConstruction = true;
+  const isUnderConstruction = false;
 
   return isUnderConstruction ? (
     <UnderConstruction />
   ) : (
     <MenuProvider>
-      <div className={classes.layout}>
+      <div className={`${classes.layout} ${centuryGothicFont.className}`}>
         <Sidebar />
         <MenuOpenButton />
         <main className={classes.layout_main}>{children}</main>
--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
   swcMinify: true,
   trailingSlash: true,
   images: {
-    // formats: ['image/avif', 'image/webp', 'image/png'],
+    formats: ['image/avif', 'image/webp'],
   },
 };
 
